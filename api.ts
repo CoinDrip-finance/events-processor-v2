da0f1888b77e40505ed84f1577c1f3f35ca834d3
Razvan Statescu <razvan@statescu.net>
2023-05-30 14:43:15 +0300

    update

--- api.ts
+++ api.ts
@@ -10,22 +10,11 @@ export const getTokenData = async (tokenIdentifier: string) => {
     };
   }
   const {
-    data: {
-      data: {
-        data: { returnData },
-      },
-    },
-  } = await axios.post(`${process.env.GATEWAY_URL}/vm-values/query`, {
-    scAddress: "erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u",
-    funcName: "getTokenProperties",
-    args: [Buffer.from(tokenIdentifier, "utf-8").toString("hex")],
-  });
-
-  const tokenName = Buffer.from(returnData[0], "base64").toString("utf-8");
-  const decimals = parseInt(Buffer.from(returnData[5], "base64").toString("utf-8").replace("NumDecimals-", ""));
+    data: { name, decimals },
+  } = await axios.get(`${process.env.API_URL}/tokens/${tokenIdentifier}`);
 
   return {
     decimals,
-    payment_token_label: tokenName,
+    payment_token_label: name,
   };
 };
